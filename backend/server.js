467eac6665699b5a8d8db1c9c649d1e6085aab71
desarrollo-gestion-smart <desarrollo.gestion.smart@gmail.com>
2024-12-03 07:15:40 -0300

    authrequired

--- backend/server.js
+++ backend/server.js
@@ -39,6 +39,28 @@ app.use('/api',authRoutes);
 
 //wallets
 
+
+const authrequired = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(401).json({ message: "No autorizado" });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    try {
+        const decoded = jwt.verify(token, process.env.TOKEN_SECRET );
+        req.user = decoded;
+        next();
+    } catch (error) {
+        console.error('Token inválido:', error.message);
+        res.status(403).json({ message: "Token inválido o expirado" });
+    }
+};
+
+
+
 app.get('/api/mercadopago/callback', authrequired, async (req, res) => {
   const { code } = req.query;
 
